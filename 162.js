4f9f25522948e9d0046bde65b41719e64475e6b8
coderpawz <coderpawz@gmail.com>
2016-07-25 21:21:48 -0400

    alternative for findPeakElement

--- 162.js
+++ 162.js
@@ -31,6 +31,26 @@ var findPeakElement = function(nums) {
   return -1;
 };
 
+// public int findPeak(int[] A) {
+//     // write your code here
+//     int start = 1, end = A.length-2; // 1.答案在之间，2.不会出界
+//     while(start + 1 <  end) {
+//         int mid = (start + end) / 2;
+//         if(A[mid] < A[mid - 1]) {
+//             end = mid;
+//         } else if(A[mid] < A[mid + 1]) {
+//             start = mid;
+//         } else {
+//             end = mid;
+//         }
+//     }
+//     if(A[start] < A[end]) {
+//         return end;
+//     } else {
+//         return start;
+//     }
+// }
+
 console.log('2', findPeakElement([1,2,3,1]))
 console.log()
 
