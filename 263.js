7030553abe9c0ee70e153658ed24b6a9e55cf3d8
coderpawz <coderpawz@gmail.com>
2016-08-21 16:57:34 -0400

    ugly number

---
+++ 263.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+var isUgly = function(num) {
+  if (!num) {
+    return false;
+  }
+  if (num === 1) {
+    return true;
+  }
+
+  const nums = [2,3,5];
+  for (var i = nums.length - 1; i >= 0; i--) {
+    if (num % nums[i] === 0) {
+      return isUgly(num / nums[i]);
+    }
+  }
+  return false;
+};
+
+console.log(true, isUgly(6));
+console.log(true, isUgly(8));
+console.log(false, isUgly(14));
