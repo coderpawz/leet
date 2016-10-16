376ed8decc83c16bc2e26eb92f931bbb70588781
coderpawz <coderpawz@gmail.com>
2016-10-16 14:47:12 -0400

    copy list with random nodes; no map solution

--- 138.js
+++ 138.js
@@ -5,6 +5,20 @@
  *     this.next = this.random = null;
  * }
  */
+function RandomListNode(label) {
+    this.label = label;
+    this.next = this.random = null;
+}
+
+function print(head) {
+    let curr = head;
+    const res = [];
+    while (curr) {
+        res.push(curr.random ? curr.random.label : '#');
+        curr = curr.next;
+    }
+    return res.join(', ');
+}
 
 /**
  * @param {RandomListNode} head
@@ -17,32 +31,59 @@ var copyRandomList = function(head) {
         return null;
     }
 
-    const nodes = [head];
+    const nodes = [];
     const rands = [];
-    const newNodes = [Object.assign({}, head, {next: null, random: null})];
+    const newNodes = [];
     let curr = head;
-    while(curr.next) {
-        nodes.push(curr.next);
-        newNodes.push(Object.assign({}, curr.next, {next: null, random: null}));
+    while(curr) {
+        nodes.push(curr);
+        newNodes.push(new RandomListNode(curr.label));
         curr = curr.next;
     }
-    rands.push(nodes.indexOf(head.random));
     curr = head;
-    while(curr.next) {
-        rands.push(nodes.indexOf(curr.next.random));
+    while(curr) {
+        rands.push(nodes.indexOf(curr.random));
         curr = curr.next;
     }
-    for(let ii = 0, rand = rands[ii]; ii < nodes.length; ii++) {
+    for(let ii = 0; ii < nodes.length; ii++) {
         newNodes[ii].next = newNodes[ii+1] || null;
-        newNodes[ii].random = rand === -1 ? null : newNodes[rand];
+        newNodes[ii].random = rands[ii] === -1 ? null : newNodes[rands[ii]];
     }
     return newNodes[0];
 };
 
-// var d = {d:123, next: null, random: null};
-// var c = {c:123, next: d, random: null};
-// var b = {b:123, next: c, random: d};
-var a = {label:-1, next: null, random: null};
+const a = new RandomListNode(-1);
+const b = new RandomListNode(0);
+const c = new RandomListNode(1);
+const d = new RandomListNode(2);
+const e = new RandomListNode(3);
+const f = new RandomListNode(4);
+const g = new RandomListNode(-3);
+const h = new RandomListNode(8);
+const i = new RandomListNode(7);
+const j = new RandomListNode(7);
 
-console.log(a);
-console.log(copyRandomList(a));
+a.random = a;
+b.random = h;
+c.random = i;
+d.random = g;
+e.random = f;
+f.random = f;
+g.random = g;
+h.random = null;
+i.random = null;
+j.random = a;
+
+a.next = b;
+b.next = c;
+c.next = d;
+d.next = e;
+e.next = f;
+f.next = g;
+g.next = h;
+h.next = i;
+i.next = j;
+j.next = null;
+
+console.log(print(a));
+console.log(print(copyRandomList(a)));
