fe5b407c7b097548c913ce88be8c2b53205ad5d0
coderpawz <coderpawz@gmail.com>
2016-11-24 23:37:05 -0500

    valid paranthesis

---
+++ valid-parantheses.js
@@ -0,0 +1,42 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+  const stack = [];
+  let ch = '';
+  let closing = false;
+  let matching = false;
+  for (var ii = 0; ii < s.length; ii++) {
+    ch = s.charAt(ii);
+    closing = isClosing(ch);
+    matching = isMatching(stack[stack.length - 1], ch);
+    if (!stack.length && closing) {
+      return false;
+    }
+    if (!matching && closing) {
+      return false;
+    }
+    if (matching) {
+      stack.pop();
+    } else {
+      stack.push(ch);
+    }
+  }
+  return !stack.length;
+};
+
+function isMatching(l, r) {
+  return l === '(' && r === ')' ||
+    l === '[' && r === ']' ||
+    l === '{' && r === '}';
+}
+
+function isClosing(ch) {
+  return ch === ')' || ch === ']' || ch === '}';
+}
+
+console.log(true, isValid("()"));
+console.log(true, isValid("()[]{}"));
+console.log(false, isValid("(]"));
+console.log(false, isValid("([)]"));
